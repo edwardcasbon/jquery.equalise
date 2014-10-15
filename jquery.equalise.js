9e18b5bcd6a5a03bfc06be1941d166089ebff204
Edward Casbon <edward@edwardcasbon.co.uk>
2014-10-15 15:24:16 +0100

    Updated equalise to use groups
    Utilising data attributes to allow nested equalising using groups.
    
    E.g. add equalise functionality to a container and equalise headings
    and content separately, using data attributes on the items themselves.

--- jquery.equalise.js
+++ jquery.equalise.js
@@ -3,11 +3,14 @@
 * Author: Edward Casbon
 * Email: edward@edwardcasbon.co.uk
 * URL: http://www.edwardcasbon.co.uk
-* Version: 2.0
-* Date: 24th September 2014
+* Version: 2.1
+* Date: 15th October 2014
 *
 * Example usage:
-* $(".column-container").equalise();
+* $(".column-container").equalise({
+*   itemClass: "equalise-item",
+*   groupAttr: "equalise-group"	
+* });
 *
 *
 **/
@@ -16,36 +19,47 @@
 
 		// Default settings.
 		var settings = {
-			itemClass: "equalise-item"
+			itemClass: "equalise-item",
+			groupAttr: "equalise-group"
 		};
-
+		
 		// Overide with custom settings.
 		$.extend(settings, options);
 
 		// Loop through the elements.
 		return this.each(function(){
 			var $this = $(this),
-				height = 0,
-				items = $this.find("." + settings.itemClass);
+				items = $this.find("." + settings.itemClass),
+				groups = [];
 
 			// Loop through the items, working out the largest height.
 			for(var i=0; i<items.length; i++) {
 				var $item = $(items[i]),
-					itemHeight = $item.outerHeight();
-
-				height = (itemHeight > height) ? itemHeight : height;
+					itemHeight = $item.outerHeight(),
+					groupAttr = "data-" + settings.groupAttr,
+					group = $item.attr(groupAttr) || "default";
+				
+				// If group not yet added to array, add and set to 0.
+				if(!groups[group]) {
+					groups[group] = 0;
+				};
+				
+				// Set the largest height of the group.
+				groups[group] = (itemHeight > groups[group]) ? itemHeight : groups[group];				
 			}
-
+			
 			// Loop through the items setting the heights.
 			for(var i=0; i<items.length; i++) {
-				var $item = $(items[i]);
+				var $item = $(items[i]),
+					groupAttr = "data-" + settings.groupAttr,
+					itemGroup = $item.attr(groupAttr) || "default";
+					
 				$item.height(
-					height -
+					groups[itemGroup] -
 					$item.css("paddingTop").replace(/[^-\d\.]/g, '') -
 					$item.css("paddingBottom").replace(/[^-\d\.]/g, '')
 				);
 			}
-
 		});
 	};
 })(jQuery);
