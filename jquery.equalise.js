053ecde660ce1d621c7d17d89c74d1e96db19a13
Edward Casbon <edward@edwardcasbon.co.uk>
2016-03-07 15:51:24 +0000

    Updated comments and minified.

--- jquery.equalise.js
+++ jquery.equalise.js
@@ -9,7 +9,7 @@
 * Example usage:
 * $(".column-container").equalise({
 *   itemClass: "equalise-item",
-*   groupAttr: "equalise-group"	
+*   groupAttr: "equalise-group"
 * });
 *
 *
@@ -22,7 +22,7 @@
 			itemClass: "equalise-item",
 			groupAttr: "equalise-group"
 		};
-		
+
 		// Overide with custom settings.
 		$.extend(settings, options);
 
@@ -39,27 +39,27 @@
 				groupAttr = "data-" + settings.groupAttr,
 				group = $item.attr(groupAttr) || "default";
 
-				// remove any previously set heights
+				// Remove any previously set heights.
 				$item.height('auto');
 
-				// get natural height
+				// Get the elements natural height.
 				itemHeight = $item.outerHeight();
-				
+
 				// If group not yet added to array, add and set to 0.
 				if(!groups[group]) {
 					groups[group] = 0;
 				};
-				
+
 				// Set the largest height of the group.
-				groups[group] = (itemHeight > groups[group]) ? itemHeight : groups[group];				
+				groups[group] = (itemHeight > groups[group]) ? itemHeight : groups[group];
 			}
-			
+
 			// Loop through the items setting the heights.
 			for(var i=0; i<items.length; i++) {
 				var $item = $(items[i]),
 					groupAttr = "data-" + settings.groupAttr,
 					itemGroup = $item.attr(groupAttr) || "default";
-					
+
 				$item.height(
 					groups[itemGroup] -
 					$item.css("paddingTop").replace(/[^-\d\.]/g, '') -
